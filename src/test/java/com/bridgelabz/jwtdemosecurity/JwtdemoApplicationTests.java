3d93e2de60322843cf9c2ce54b602247d6d86eef
nikitha chimmmula <nikithachimmula97@gmail.com>
2022-08-03 19:20:56 +0530

    JWT_SIMPLE_LOGIN

---
+++ src/test/java/com/bridgelabz/jwtdemosecurity/JwtdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.jwtdemosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
